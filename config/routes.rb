298d43a2bdd403f30effee1a775a01b287e595e2
marine payet <marine.payet3@gmail.com>
2020-11-10 13:58:26 +0100

    exo2 en cours

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
   # display all
+  get "tasks/new", to:"tasks#new"
+  # create
+  post "tasks", to: "tasks#create"
+  # send to db
   get "tasks/:id", to: "tasks#show", as: :task
   # display one
+
 
 end
